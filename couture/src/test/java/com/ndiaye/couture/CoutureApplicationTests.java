49242785287570dcc38a635ef9a126377fab3d70
fadibou <ndiayef99@gmail.com>
2020-09-09 22:12:43 +0200

    first commit

---
+++ couture/src/test/java/com/ndiaye/couture/CoutureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndiaye.couture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoutureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
